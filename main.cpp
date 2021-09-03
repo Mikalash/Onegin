207e076ff9f6d6d3fafda673bff648d79a152826
Mikalash <89709569+Mikalash@users.noreply.github.com>
2021-09-04 02:28:31 +0300

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -2,45 +2,78 @@
 #include <math.h>
 #include <assert.h>
 #include <windows.h>
-//#include <cfloat>
+#include <cfloat>
 
-const int NUM_SLOTS = 10000;
+const int NUM_SLOTS = 1000;
 
-///выводит строку и переходит на новую строку
-int f_puts(char s[]);                                         //ok
-///возвращает длину строки оканчивающейся нулевым символом
-int f_strlen(char s[]);                                       //ok
-///копирует строку s2 в строку s1
-int f_strcpy(char s1[], char s2[]);                           //ok
-///добавляет строку s2 в конец строки s1
-int f_strcat(char s1[], char s2[]);                           //ok
-///считывает файл stream до n-1 символов
-///пока не встретиться символ новой строки или EOF и записывает их в массив s
-int f_fgets(char *s[], int n, FILE* stream);                  //gon't understend
-///дублирует строку
-char* f_strdup(char s[]);                                     //ok
+///РІС‹РІРѕРґРёС‚ СЃС‚СЂРѕРєСѓ Рё РїРµСЂРµС…РѕРґРёС‚ РЅР° РЅРѕРІСѓСЋ СЃС‚СЂРѕРєСѓ
+int f_puts(char s[]);
+///РІРѕР·РІСЂР°С‰Р°РµС‚ РґР»РёРЅСѓ СЃС‚СЂРѕРєРё РѕРєР°РЅС‡РёРІР°СЋС‰РµР№СЃСЏ РЅСѓР»РµРІС‹Рј СЃРёРјРІРѕР»РѕРј
+int f_strlen(char s[]);
+///РєРѕРїРёСЂСѓРµС‚ СЃС‚СЂРѕРєСѓ s2 РІ СЃС‚СЂРѕРєСѓ s1
+int f_strcpy(char s1[], char s2[]);
+///РґРѕР±Р°РІР»СЏРµС‚ СЃС‚СЂРѕРєСѓ s2 РІ РєРѕРЅРµС† СЃС‚СЂРѕРєРё s1
+int f_strcat(char s1[], char s2[]);
+///СЃС‡РёС‚С‹РІР°РµС‚ С„Р°Р№Р» stream РґРѕ n-1 СЃРёРјРІРѕР»РѕРІ
+///РїРѕРєР° РЅРµ РІСЃС‚СЂРµС‚РёС‚СЊСЃСЏ СЃРёРјРІРѕР» РЅРѕРІРѕР№ СЃС‚СЂРѕРєРё РёР»Рё EOF Рё Р·Р°РїРёСЃС‹РІР°РµС‚ РёС… РІ РјР°СЃСЃРёРІ s
+int f_fgets(char s[], int n, FILE* stream);
+///РґСѓР±Р»РёСЂСѓРµС‚ СЃС‚СЂРѕРєСѓ
+char* f_strdup(char s[]);
+
+///1 РµСЃР»Рё s1 СЂР°РЅСЊС€Рµ, 2 РµСЃР»Рё s2 СЂР°РЅСЊС€Рµ
+int sortirovka(char s1[], char s2[]);
 
 int main()
 {
-    char s1[NUM_SLOTS] = "lol kek cheburek";
-    char s2[NUM_SLOTS] = "ded really cool prepod";
+    char s[NUM_SLOTS][NUM_SLOTS];
+    int num_strings = 0;
+    
+    //РІРІРѕРґ РІ РјР°СЃСЃРёРІ
+    FILE* input_file = fopen("qwe.txt","r");
+    int ret = 0;
+    while (ret == 0)
+    {
+        ret = f_fgets(s[num_strings], NUM_SLOTS, input_file);
+        num_strings++;
+    }
+    fclose(input_file);
+    
+    //РїСЂРѕРІРµСЂРєР°
+    for (int i = 0; i < num_strings; i++)
+        printf("%s\n", s[i]);
+    printf("\n\n%d\n\n", num_strings);
 
-    printf("%s", f_strdup(s2));
+    //СЃРѕСЂС‚РёСЂРѕРІРєР°
+    for (int i = 0; i < num_strings; i++)
+        for (int j = 0; j < (num_strings - 1); j++)
+            if (sortirovka(s[j], s[j + 1]) == 2)
+            {
+                char work[NUM_SLOTS];
+                f_strcpy(work, s[j]);
+                f_strcpy(s[j], s[j + 1]);
+                f_strcpy(s[j + 1], work);
+            }
+
+    //РїСЂРѕРІРµСЂРєР°
+    for (int i = 0; i < num_strings; i++)
+        printf("%s\n", s[i]);
 
     return 0;
 }
 
 int f_puts(char s[])
 {
-    int len_s = f_strlen(s);
+    assert(&s[0] != NULL);
 
-    for (int i = 0; i < len_s; i++)
+    for (int i = 0; s[i] != '\0'; i++)
         putchar(s[i]);
     putchar('\n');
 }
 
 int f_strlen(char s[])
 {
+    assert(&s[0] != NULL);
+
     int len = 0;
 
     while (s[len] != '\0')
@@ -51,27 +84,72 @@ int f_strlen(char s[])
 
 int f_strcpy(char s1[], char s2[])
 {
-    int len_s2 = f_strlen(s2);
+    assert(&s1[0] != NULL);
+    assert(&s2[0] != NULL);
+    assert(&s1[0] != &s2[0]);
 
-    for (int i = 0; i < len_s2; i++)
+    int i;
+    for (i = 0; s2[i] != '\0'; i++)
         s1[i] = s2[i];
+
+    s1[i] = '\0';
 }
 
 int f_strcat(char s1[], char s2[])
 {
-    int len_s1 = f_strlen(s1);
-    int len_s2 = f_strlen(s2);
+    assert(&s1[0] != NULL);
+    assert(&s2[0] != NULL);
+    assert(&s1[0] != &s2[0]);
 
-    for (int i = len_s1; i < (len_s1 + len_s2); i++)
-        s1[i] = s2[i - len_s1];
+    int len_s1 = f_strlen(s1);
+
+    for (int i = 0; s2[i] != '\0'; i++)
+        s1[i + len_s1] = s2[i];
 }
 
-int f_fgets()
+int f_fgets(char s[], int n, FILE* stream)
 {
+    assert(&s[0] != NULL);
+    assert(stream != NULL);
 
+    int i;
+    int in;
+    for (i = 0; i < n; i++)
+    {
+        in = getc(stream);
+        if (in != '\n' && in != EOF)
+            s[i] = in;
+        else
+            i = n;
+    }
+    s[i] = '\0';
+
+    if (in == EOF)
+        return -1;
+    return 0;
 }
 
 char* f_strdup(char s[])
 {
+    assert(&s[0] != NULL);
+
     return &s[0];
+}
+
+int sortirovka(char s1[], char s2[])
+{
+    assert(&s1[0] != NULL);
+    assert(&s2[0] != NULL);
+    assert(&s1[0] != &s2[0]);
+
+    int length = min(f_strlen(s1), f_strlen(s2));
+    for (int i = 0; i < length; i++)
+    {
+        char a = s1[i];
+        char b = s2[i];
+        if (a < b)
+            return 1;
+        if (a > b)
+            return 2;
+    }
 }
